4b4eba602e43158495559db3f2f1c598c1e77192
mwebber3 <mwebber3@users.noreply.github.com>
2017-07-25 20:43:23 -0500

    Added the scripts for the project

---
+++ Pacdot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacdot : MonoBehaviour
+{
+
+	void OnTriggerEnter2D (Collider2D co)
+	{
+		if (co.name == "pacman")
+			Destroy (gameObject);
+	}
+}
